9a83a947ad093cfd6d40cd3311309ce525df4192
Scott Kuhl <scott@kuhl.ws>
2018-09-22 16:16:41 -0500

    Initial project.

--- Recursion/Program.cs
+++ Recursion/Program.cs
@@ -1,12 +1,42 @@
 ﻿using System;
 
-namespace Recursion
+namespace FamilyTree
 {
-    class Program
+    static class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            var elizabeth = new FamilyMember { Name = "Elizabeth" };
+            var charles = new FamilyMember { Name = "Charles" };
+            var anne = new FamilyMember { Name = "Anne" };
+            var andrew = new FamilyMember { Name = "Andrew" };
+            var edward = new FamilyMember { Name = "Edward" };
+            var dianna = new FamilyMember { Name = "Dianna" };
+            var camilla = new FamilyMember { Name = "Camilla" };
+            var william = new FamilyMember { Name = "William" };
+            var henry = new FamilyMember { Name = "Henry" };
+
+            elizabeth.Contacts.Add(charles);
+            elizabeth.Contacts.Add(anne);
+            elizabeth.Contacts.Add(andrew);
+            elizabeth.Contacts.Add(edward);
+
+            charles.Contacts.Add(dianna);
+            charles.Contacts.Add(camilla);
+            charles.Contacts.Add(william);
+            charles.Contacts.Add(henry);
+
+            PrintTree(elizabeth);
+            Console.ReadLine();
+        }
+
+        static void PrintTree(FamilyMember familyMember)
+        {
+            Console.WriteLine(familyMember.Name);
+            foreach (var innerFamilyMember in familyMember.Contacts)
+            {
+                PrintTree(innerFamilyMember);
+            }
         }
     }
 }
